528a9b8ac65de9fe9729ed9707fdc53510d9eaab
gpokhark <gpokhark@gmail.com>
2023-03-05 18:53:36 -0500

    Update vcgencmd location for other raspberry pi.

--- my_pi_temp.sh
+++ my_pi_temp.sh
@@ -3,4 +3,5 @@ cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
 echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+# echo "GPU => $(/usr/bin/vcgencmd measure_temp)"
 echo "CPU => $((cpu/1000))'C"

2de91713571bc621a210a6a3c83976a791b2e451
gpokhark <gpokhark@gmail.com>
2024-06-29 19:11:30 -0400

    Changed the router ip to a variable.

--- check_wifi.sh
+++ check_wifi.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # keep wifi alive
-ping -c3 192.168.0.1 #router ip address
+router_ip=$(ip route | grep default | awk '{print $3}')
+ping -c3 ${router_ip} #router ip address
 if [ $? != 0 ]; then
   echo "......"
   echo "No network connection, restarting wlan0"
